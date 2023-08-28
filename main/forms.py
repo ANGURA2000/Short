318fac7c7beb39eefafc38753597eaa335caa7c3
ANGURA2000 <emmanuelangura47@gmail.com>
2023-08-28 09:41:01 +0300

    Delete some codes

--- main/forms.py
+++ main/forms.py
@@ -1,20 +1,3 @@
-# from django import forms
-# from .models import Contact
-
-# class ContactForm(forms.ModelForm):
-#     class Meta:
-#         model = Contact
-#         fields = '__all__'
-
-
-# forms.py
-# from django import forms
-# from .models import ContactMessage
-
-# class ContactForm(forms.ModelForm):
-#     class Meta:
-#         model = ContactMessage
-#         fields = ['name', 'email', 'message']
 
 from django import forms
 from .models import ContactMessage,ReportMessage
